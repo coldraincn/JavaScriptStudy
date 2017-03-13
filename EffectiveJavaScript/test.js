1e3bdc6ba512c26a12650fffa3545f9929f61f2b
coldraincn <handan213@gmail.com>
2017-03-13 17:14:01 +0800

    18,19

--- EffectiveJavaScript/test.js
+++ EffectiveJavaScript/test.js
@@ -22,3 +22,54 @@ function test(x){
 }
 test(true);
 test(false);
+
+var co = require('co');
+var debug = require('debug')('v0')
+
+module.exports = {
+  middleware :[],
+  use: function (fn) {
+    debug('use:' + fn);
+
+    this.middleware.push(fn);
+    return this;
+  },
+  callback: function (cb) {
+    const fn = this.compose(this.middleware);
+    debug('callback compose fn = ' + fn)
+
+    co(fn).then(cb, function (err) {
+      console.error(err.stack);
+    });
+  },
+  compose: function (middleware) {
+    debug('compose=' + middleware)
+    return function *(next){
+      if (!next) {
+        next = function *(){}
+      }
+
+      var i = middleware.length;
+
+      while (i--) {
+        debug('compose middleware[' + i + ']=: ' + middleware[i])
+        // next = co.wrap(middleware[i]).call(this);
+        next = middleware[i].call(this, next);
+
+        debug(next)
+      }
+
+      return yield *next;
+    }
+  } 
+}
+function com(x,y){
+  if(x<y){
+    return -1;
+  }
+  if(x>y){
+    return 1;
+  }
+  return 0;
+}
+[3,1,4,1,5,9].sort(com);
